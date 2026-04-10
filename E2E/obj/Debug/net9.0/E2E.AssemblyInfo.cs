dcce4e00babf9e38915fa908bac0fbaaa21b834c
Ankit Shrivastava <ankitwits@gmail.com>
2026-04-10 11:53:15 +0530

    Apply gitignore and remove tracked build files

--- E2E/obj/Debug/net9.0/E2E.AssemblyInfo.cs
+++ E2E/obj/Debug/net9.0/E2E.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E2E")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a17cffbf7d3ac14ff7f091c7cbf08aa697e3e2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("E2E")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E2E")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UI/obj/Debug/net9.0/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net9.0/UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8024c90e0f78ff798c3c4f4f77561749bd1bef94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a17cffbf7d3ac14ff7f091c7cbf08aa697e3e2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
