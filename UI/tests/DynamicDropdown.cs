601b84bef459f47b072462183d1cbc4724a4aac2
Ankit Shrivastava <ankitwits@gmail.com>
2026-04-17 23:22:10 +0530

    Dynamic Dropdown Added

--- UI/tests/DynamicDropdown.cs
+++ UI/tests/DynamicDropdown.cs
@@ -6,7 +6,7 @@ using UI.Core;
 namespace UI.tests;
 
 
-public class DynamicDropDown: BaseClassTest
+public class DynamicDropDown : BaseClassTest
 {
     [Test]
     public async Task Selectitem()
@@ -14,35 +14,31 @@ public class DynamicDropDown: BaseClassTest
 
         await page.GotoAsync("https://www.yatra.com/");
         await page.WaitForTimeoutAsync(1000);
-        var popup =  page.Locator("//span[contains(@class,'style_cross')]//img[@alt='cross']").First;
+        var popup = page.Locator("//span[contains(@class,'style_cross')]//img[@alt='cross']").First;
 
         if (await popup.IsVisibleAsync())
         {
             await popup.ClickAsync();
-            
+
 
         }
 
-        await page.WaitForTimeoutAsync(5000); 
-        
-       //// await page.Locator("//Span[@class='style_cross__q1ZoV']//img[@src='/react-home/_next/static/media/close.e57bf4bf.svg']").ClearAsync();
-
-       // await page.PauseAsync();
-       await page.Locator("//*[contains(text(),'Departure From')]/following::div[@role='Combobox']").First.ClickAsync();
         await page.WaitForTimeoutAsync(5000);
-        await page.Locator("//div[@class='MuiBox-root css-8hsq1d']").ClickAsync();
-        await page.Locator("//input[@id='input-with-icon-adornment']").ClickAsync();
-        // PressSequentiallyAsync("Ban", new(){Delay=200});
-        await page.Locator("/div[@class='MuiBox-root css-134xwrj']/Ul").GetByRole(AriaRole.Listitem).Filter(new()
-          { HasTextString = "Bangalore" }).ClickAsync();
-          await page.WaitForTimeoutAsync(2000);
+
+
+        // await page.PauseAsync();
+        await page.Locator("//*[contains(text(),'Departure From')]/following::div[@role='Combobox']").First.ClickAsync();
+        await page.WaitForTimeoutAsync(2000);       
+        await page.Locator("//div[@class='MuiBox-root css-134xwrj']").GetByRole(AriaRole.Listitem).Filter(new()
+        { HasTextString = "Bangalore" }).ClickAsync();
+        await page.WaitForTimeoutAsync(2000);
 
         ////div[@class='MuiBox-root css-134xwrj']/Ul
 
 
 
 
-        
+
 
 
 
